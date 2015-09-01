b032d061797844b2f111cc0b7b8afb25da30a1cc
lc <ziyu0123456789@gmail.com>
2015-09-02 00:10:27 +0800

    GruntFile脚本增加对Sass的详细配置

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,10 +3,42 @@ module.exports = function(grunt) {
     // 配置Grunt各种模块的参数
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+
+        // Metadata.
+        meta: {
+            basePath: '../',
+            srcPath: '../assets/sass/',
+            deployPath: '../assets/css/'
+        },
+
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+            '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+            '* Copyright (c) <%= grunt.template.today("yyyy") %> ',
+
+        // Task configuration.
+        sass: {
+            dist: {
+                files: {
+                    '<%= meta.deployPath %>style.css': '<%= meta.srcPath %>style.scss'
+                },
+                options: {
+                    sourcemap: 'true'
+                }
+            }
+        },
+
+
         jshint: { /* jshint的参数 */ },
         concat: { /* concat的参数 */ },
         uglify: { /* uglify的参数 */ },
-        watch: { /* watch的参数 */ }
+        watch: { /* watch的参数 */
+            scripts: {
+                files: [
+                    '<%= meta.srcPath %>/**/*.scss'
+                ],
+                tasks: ['sass']
+            }
+        }
     });
 
     // 从node_modules目录加载模块文件
@@ -17,7 +49,7 @@ module.exports = function(grunt) {
     });
 
     // 每行registerTask定义一个任务
-    grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+    grunt.registerTask('default', ['sass', 'jshint', 'concat', 'uglify']);
     grunt.registerTask('check', ['jshint']);
 
 };
