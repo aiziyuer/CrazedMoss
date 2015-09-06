1597920947448b5dda88e75168f7d42f8b6a21a5
lc <ziyu0123456789@gmail.com>
2015-09-06 22:49:58 +0800

    watch的Compass实时编译实现

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,86 +1,55 @@
 module.exports = function(grunt) {
 
+    // 从node_modules目录加载模块文件
+    require('load-grunt-tasks')(grunt, {
+        pattern: ['grunt-contrib-*'],
+        config: 'package.json',
+        scope: 'devDependencies'
+    });
+
     // 配置Grunt各种模块的参数
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
 
-        // Metadata.
-        meta: {
-            basePath: '../',
-            srcPath: '../assets/sass/',
-            deployPath: '../assets/css/'
-        },
+        pkg: grunt.file.readJSON('package.json'),
 
         banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
             '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
             '* Copyright (c) <%= grunt.template.today("yyyy") %> ',
 
-        // Task configuration.
-        sass: {
-            dist: {
-                files: {
-                    '<%= meta.deployPath %>style.css': '<%= meta.srcPath %>style.scss'
-                },
+        compass: {
+            prod: {
+                style: 'compressed',
                 options: {
-                    sourcemap: 'true'
+                    config: 'config.rb',
+                    environment: 'production',
+                }
+            },
+            dev: {
+                sourceMap: true,
+                forcecompile: true,
+                style: 'nested',
+                options: {
+                    config: 'config.rb',
                 }
             }
         },
 
-        bower: {
-            install: {
-                options: {
-                    targetDir: './public/js/lib',
-                    layout: 'byComponent',
-                    install: true,
-                    verbose: false,
-                    cleanTargetDir: false,
-                    cleanBowerDir: false,
-                    bowerOptions: {}
-                }
-            }
-        },
-
-        jshint: { /* jshint的参数 */
+        /* watch的参数 */
+        watch: {
+            compass: {
+                files: ['src/sass/*.scss'],
+                tasks: ['compass:dev']
+            },
             options: {
-                curly: true,
-                eqeqeq: true,
-                immed: true,
-                latedef: true,
-                newcap: true,
-                noarg: true,
-                sub: true,
-                undef: true,
-                unused: true,
-                boss: true,
-                eqnull: true,
-                browser: true,
-                globals: {
-                    jQuery: true
-                }
-            }
-        },
-        concat: { /* concat的参数 */ },
-        uglify: { /* uglify的参数 */ },
-        watch: { /* watch的参数 */
-            scripts: {
-                files: [
-                    '<%= meta.srcPath %>/**/*.scss'
-                ],
-                tasks: ['sass']
+                spawn: false,
             }
         }
     });
 
-    // 从node_modules目录加载模块文件
-    require('load-grunt-tasks')(grunt, {
-        pattern: ['grunt-contrib-*'],
-        config: '../package.json',
-        scope: 'devDependencies'
+    grunt.registerTask('dev', function(target) {
+        //TODO 清理
+        //TODO 全量编译
+        //TODO 启动watch
+        ''
     });
-
-    // 每行registerTask定义一个任务
-    grunt.registerTask('default', ['sass', 'jshint', 'concat', 'uglify']);
-    grunt.registerTask('check', ['jshint']);
-
 };
